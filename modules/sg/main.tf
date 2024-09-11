3556f865e046f0d26ca172b19b4822aee7900700
BISSOMBOLO <siegab@yahoo.fr>
2024-09-11 15:26:09 +0200

    first commit

---
+++ modules/sg/main.tf
@@ -0,0 +1,46 @@
+resource "aws_security_group" "my_security_group" {
+  name        = "var.sg_name"
+  description = "Allow http and https inbound traffic"
+  #vpc_id      = "var.vpc_id"
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }  
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = var.aws_sg_tag 
+
+  }
+  
+}
+
---
+++ modules/sg/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_sg_name" {
+  type        = string
+  
+}
+variable "aws_sg_tag" {
+  type        = string
+  description = "set security group tag"
+  
+}
+
